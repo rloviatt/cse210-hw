c64d82fbf5b43699c756a7938daad5af59491725
rloviatt <ovi21001@byui.edu>
2024-10-04 18:58:20 -0600

    prepare02 complete.

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -5,7 +5,7 @@ class Program
 {
     static void Main(string[] args)
     {
-        List<int> numbers = new List<int>();
+        List<int> numbers = new();
         int userInt = 1;
         Console.WriteLine("Enter a list of numbers, type 0 when finished.");
         while (userInt != 0)
