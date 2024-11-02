3899c7c81094183704327ba979bc95b9e44d615b
rloviatt <ovi21001@byui.edu>
2024-11-01 19:48:20 -0600

    Inheritance Learning Activity Done

--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -4,6 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop04 World!");
+        int i = 0;
+        
+        while (i < 5)
+        {
+            Console.Write("+");
+
+            Thread.Sleep(500);
+
+            Console.Write("\b \b"); // Erase the + character
+            Console.Write("-"); // Replace it with the - character
+            Thread.Sleep(500);
+            Console.Write("\b \b"); // Erase the - character
+
+            i+=1;
+        }
     }
 }
